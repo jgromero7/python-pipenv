e1823a5ab011c166c809db1fe0b617db57d5a648
jromero7 <josegromero7@gmail.com>
2019-08-19 13:13:20 -0500

    Init Repository

---
+++ index.py
@@ -0,0 +1,24 @@
+from colorama import Fore, Style
+
+print(Fore.RED + 'Hello World')
+print(Style.RESET_ALL)
+
+# Instal pipenv
+# $ pip3 install pipenv
+# Init pipenv
+# $ pipenv shell
+
+# Other Command
+# Install Dependencies 
+# $ pipenv install name_module
+# Install Dev Dependencies 
+# $ pipenv install name_module --dev
+# Uninstall Dependencies
+# $ pipenv uninstall name_module
+# View Dependencies Install
+# $ pipenv lock -r
+# View Threads Dependencies
+# $ pipenv graph
+# Install Requirements - Review File => requirements.txt
+# $ pipenv intall -r requirements.txt
+
